d0401c629c97594f3fdc544db9f4bbab4cb51136
Kiran Sahu <61940769+kiransahu-dev@users.noreply.github.com>
2023-04-16 10:42:06 +0530

    updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import store from '../reducers';
+import { increment, decrement } from '../actions/action';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const count = useSelector((state) => state);
+  // console.log(JSON.stringify(count));
+  const dispatch = useDispatch();
 
   return (
     <div id='main'>
-      
+      <div data-testid='counter'>{JSON.stringify(count)}</div>
+      <br />
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
